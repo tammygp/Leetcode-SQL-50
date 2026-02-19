9fe360c1f0e5115c99e5ba99e5baa07842d834fd
tammygp <163815280+tammygp@users.noreply.github.com>
2026-02-19 00:21:22 -0800

    Create 14-1934-confirmation-rate.sql

---
+++ 14-1934-confirmation-rate.sql
@@ -0,0 +1,39 @@
+/*
+Table: Signups
+
++----------------+----------+
+| Column Name    | Type     |
++----------------+----------+
+| user_id        | int      |
+| time_stamp     | datetime |
++----------------+----------+
+user_id is the column of unique values for this table.
+Each row contains information about the signup time for the user with ID user_id.
+ 
+
+Table: Confirmations
+
++----------------+----------+
+| Column Name    | Type     |
++----------------+----------+
+| user_id        | int      |
+| time_stamp     | datetime |
+| action         | ENUM     |
++----------------+----------+
+(user_id, time_stamp) is the primary key (combination of columns with unique values) for this table.
+user_id is a foreign key (reference column) to the Signups table.
+action is an ENUM (category) of the type ('confirmed', 'timeout')
+Each row of this table indicates that the user with ID user_id requested a confirmation message at time_stamp and that confirmation message was either confirmed ('confirmed') or expired without confirming ('timeout').
+ 
+The confirmation rate of a user is the number of 'confirmed' messages divided by the total number of requested confirmation messages. The confirmation rate of a user that did not request any confirmation messages is 0. Round the confirmation rate to two decimal places.
+
+Write a solution to find the confirmation rate of each user.
+*/
+
+select s.user_id,
+    round(coalesce(sum(case when action='confirmed' then 1 else 0 end)  /
+        count(c.time_stamp) ,0),2) as confirmation_rate
+from Signups as s
+left join Confirmations as c
+on s.user_id = c.user_id
+group by user_id
