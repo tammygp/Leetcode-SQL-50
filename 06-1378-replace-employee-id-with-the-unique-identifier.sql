0747ff2d0aa8b01bcdd90e14e8e5659282b6836a
tammygp <163815280+tammygp@users.noreply.github.com>
2026-02-19 00:12:49 -0800

    Create 06-1378-replace-employee-id-with-the-unique-identifier.sql

---
+++ 06-1378-replace-employee-id-with-the-unique-identifier.sql
@@ -0,0 +1,31 @@
+/*
+Table: Employees
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains the id and the name of an employee in a company.
+ 
+
+Table: EmployeeUNI
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| unique_id     | int     |
++---------------+---------+
+(id, unique_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id and the corresponding unique id of an employee in the company.
+ 
+Write a solution to show the unique ID of each user, If a user does not have a unique ID replace just show null.
+*/
+
+select unique_id, name
+from Employees as e
+left join EmployeeUNI as eu
+on e.id = eu.id
