90b28c3d1a9c2dd51c32629c49d6150b31959596
tammygp <163815280+tammygp@users.noreply.github.com>
2026-02-19 22:05:06 -0800

    Create 21-1174-immediate-food-delivery-ii.sql

---
+++ 21-1174-immediate-food-delivery-ii.sql
@@ -0,0 +1,34 @@
+/*
+Table: Delivery
+
++-----------------------------+---------+
+| Column Name                 | Type    |
++-----------------------------+---------+
+| delivery_id                 | int     |
+| customer_id                 | int     |
+| order_date                  | date    |
+| customer_pref_delivery_date | date    |
++-----------------------------+---------+
+delivery_id is the column of unique values of this table.
+The table holds information about food delivery to customers that make orders at some date and specify a preferred delivery date (on the same order date or after it).
+ 
+
+If the customer's preferred delivery date is the same as the order date, then the order is called immediate; otherwise, it is called scheduled.
+
+The first order of a customer is the order with the earliest order date that the customer made. It is guaranteed that a customer has precisely one first order.
+
+Write a solution to find the percentage of immediate orders in the first orders of all customers, rounded to 2 decimal places.
+*/
+
+select
+    round(
+        100 * ( sum(if(order_date = customer_pref_delivery_date,1,0)) / count(*) )
+        ,2) as immediate_percentage
+from Delivery
+where (customer_id, order_date) in
+    (select
+        customer_id,
+        min(order_date)
+    from Delivery
+    group by 1 
+    )
