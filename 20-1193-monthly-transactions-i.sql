6a5fe23e9208b9673a6a0fa35ae5f92a8d375819
tammygp <163815280+tammygp@users.noreply.github.com>
2026-02-19 21:48:53 -0800

    Create 20-1193-monthly-transactions-i.sql

---
+++ 20-1193-monthly-transactions-i.sql
@@ -0,0 +1,28 @@
+/*
+Table: Transactions
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| country       | varchar |
+| state         | enum    |
+| amount        | int     |
+| trans_date    | date    |
++---------------+---------+
+id is the primary key of this table.
+The table has information about incoming transactions.
+The state column is an enum of type ["approved", "declined"].
+ 
+Write an SQL query to find for each month and country, the number of transactions and their total amount, the number of approved transactions and their total amount.
+*/
+
+select
+    date_format(trans_date,'%Y-%m') as month,
+    country,
+    count(id) as trans_count,
+    sum(case when state = 'approved' then 1 else 0 end) as approved_count,
+    sum(amount) as trans_total_amount,
+    sum(case when state = 'approved' then amount else 0 end) as approved_total_amount
+from Transactions
+group by 1,2
